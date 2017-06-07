14adf75b210f13c16d82864d58d4d1c594df31a5
root <root@localhost.localdomain>
2017-06-07 22:26:52 +0200

    Base version

---
+++ snooper.js
@@ -0,0 +1,57 @@
+function clean_form(area) {
+  $('#keywords').val('');
+  $('#mail').val('');
+  if (area == 2) {
+    $("#tbody_result").empty();
+  }
+  if (area == 1) {
+    grecaptcha.reset();
+  }
+  $('input[name=newsletter]:checked').prop( "checked", false );
+  $('input[name=service]:checked').prop( "checked", false );
+}
+function add_result(tab) {
+  for (var i = 0; i < tab.length; i++) {
+    $('#resultat_tab > tbody:last').append("<tr><td>"+tab[i]+"</td></tr>");
+  }
+}
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  // I do a clean form
+  clean_form(2);
+  // When form is submit I do ...
+  $('#form_snoop').submit(function(event) {
+    // Take all information from form
+    keywords = $('#keywords').val();
+    mail = $('#mail').val();
+    newsletter = $('input[name=newsletter]:checked').val();
+    service = $('input[name=service]:checked').val();
+    captcha_response = grecaptcha.getResponse();
+    if (captcha_response != '') {
+      $.post('snooper.php', {keywords: keywords, mail: mail,newsletter: newsletter, service: service, captcha_response: captcha_response }, function(data) {
+      //Write the result
+        data = jQuery.parseJSON(data);
+        $('#add').show();
+        add_result(data);
+        $('#resultat').show();
+        clean_form(1);
+        $(window).scrollTop($('#resultat').offset().top);
+      });
+    }
+    // I give a request to process 
+    return false;
+  });
+  $('#close').click(function(event) {
+    $('#add').hide();
+  });
+  $('#form_add').submit(function(event) {
+    q1 = $('input[name=add1]:checked').val();
+    q2 = $('input[name=add2]:checked').val();
+    $.post('add_form.php', {q1: q1, q2: q2}, function(data) {
+      jQuery.parseJSON(data);
+      console.log(data);
+      $('#add').hide(); // hide the add after send
+    });
+    return false;
+  });
+});
